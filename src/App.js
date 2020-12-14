594b734c813afcc243c94c664b8578da6783be88
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-12-14 01:47:59 -0300

    chore: add type icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const App = () => {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <h1>hello world!</h1>
     </div>
   );
-}
+};
 
 export default App;
